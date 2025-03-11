bb44d29daf9ba2ce55cabbee7588caacab8c4e7e
Bruna Junqueira Lopes <99977808+brunaafl@users.noreply.github.com>
2025-03-11 16:17:09 +0100

    Update plot_utils.py

--- util/plot_utils.py
+++ util/plot_utils.py
@@ -1,9 +1,10 @@
+import torch
 from matplotlib import pyplot as plt
 
 from util.img_utils import clear_color
 
 
-def plot_x_z_reconstruction(Z_step, X_step, step):
+def show_reconstruction(Z_step, X_step, step):
     # At the step {step} of a gibbs sampling pipeline
     # Plot estimated X and Z
     fig, axes = plt.subplots(1, 2, figsize=(20, 20))
@@ -13,8 +14,29 @@ def plot_x_z_reconstruction(Z_step, X_step, step):
     axes[0].axis('off')
 
     axes[1].imshow(clear_color(Z_step))
-    axes[1].set_title('Z Reconstructed image - step {step}')
+    axes[1].set_title(f'Z Reconstructed image - step {step}')
     axes[1].axis('off')
 
     return fig, axes
 
+def confidence_interval(data, C=0.9):
+
+    # Percentiles of the gaussian
+    X_lower = torch.quantile(data,(1-C)/2, dim=0)
+    X_upper = torch.quantile(data, (C+1)/2, dim=0)
+    X_std = torch.std(data, dim=0)
+
+    fig, ax = plt.subplots(1,2)
+
+    # Heatmap of 95% CI
+    ax[0].imshow(X_upper - X_lower, cmap='magma')
+    ax[0].title(f"{int(100*C)}% Confidence Interval")
+    ax[0].axis("off")
+
+    # standard deviation
+    ax[1].imshow(X_std, cmap='inferno')
+    ax[1].title("Standard Deviation on each pixel")
+    ax[1].axis("off")
+
+    plt.tight_layout()
+    plt.show()
