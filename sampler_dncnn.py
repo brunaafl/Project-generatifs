f0b4a00f1dc8b35a3ca526379080546086bd15b9
Bruna Junqueira Lopes <99977808+brunaafl@users.noreply.github.com>
2025-03-19 14:49:41 +0100

    Updating PnP-PGD

--- sampler_dncnn.py
+++ sampler_dncnn.py
@@ -1,41 +1,67 @@
 import torch
 from matplotlib import pyplot as plt
+from scipy.fft import fft2
+
+from util.img_utils import psnr, clear_color
 
 
-def create_sampler_dn(tau, nu, s):
-    return DnCNNsampler(tau=tau, nu=nu, s=s)
+def create_sampler_dn(tau, nu, s, operator, s_decay=0.8, normalize=True):
+    return DnCNNsampler(tau=tau, nu=nu, s=s, operator=operator, s_decay=s_decay, normalize=normalize)
 
 class DnCNNsampler:
-    def __init__(self, tau, nu, s):
+    def __init__(self, tau, nu, s, operator, s_decay=0.8, normalize=True):
         self.tau = tau
         self.nu = nu
         self.s = s
-        self.residuals = []
+        self.operator = operator
+        self.s_decay = s_decay
+        self.normalize = normalize
 
-    def set_y(self, y):
-        self.y = y
+        self.residuals = []
+        self.psnr = []
+
+    def set_y_x0(self, y, x0):
+        self.y = y.clone()
         self.normxinit = torch.linalg.vector_norm(y)
 
-    def fidelity(self, x, operator):
-        Hx = operator(x)
+        self.x0 = x0
+
+    def fidelity(self, x):
+        Hx = self.operator.forward(x)
         return torch.sum((Hx - self.y) ** 2) / (2 * self.nu ** 2)
 
     def plot_residuals(self):
-        plt.semilogy(self.residuals)
+        fig, axs = plt.subplots()
+        axs.semilogy(self.residuals)
         plt.title('Residual Norm')
         plt.show()
 
-    def sample(self, x, model, operator):
-        x_t = x.clone().requires_grad_(True)
-        fx = self.fidelity(x_t, operator)
+    def sample(self, x, model, niter=100, plot=False):
+        s = self.s
 
-        grad_fx = torch.autograd.grad(fx, x_t)[0]
-        with torch.no_grad():
-            x_aux = x_t - self.tau * grad_fx
-            Dx = model(x_aux, sigma=self.s)
-            x = Dx
+        for it in range(niter):
 
-        res = torch.linalg.vector_norm(x - x_t) / self.normxinit
-        self.residuals.append(res.cpu())
+            s = s * self.s_decay ** it
+
+            x_t = x.clone().requires_grad_(True)
+            fx = self.fidelity(x_t)
+
+            grad_fx = torch.autograd.grad(fx, x_t)[0]
+            with torch.no_grad():
+                x_aux = x_t - self.tau * grad_fx
+                Dx = model(x_aux, sigma=s)
+                x = Dx
+
+            res = torch.linalg.vector_norm(x - x_t) / self.normxinit
+            self.residuals.append(res.detach().item())
+            self.psnr.append(psnr(self.x0,x))
+
+            if plot:
+                if it % 10 == 0:
+                    fig, ax = plt.subplots()
+    
+                    ax.imshow(clear_color(x,normalize=self.normalize))
+                    ax.axis('off')
+                    plt.show()
 
         return x
