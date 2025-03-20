f782674ab1991076cd841654c012f354c235b0cc
Bruna Junqueira Lopes <99977808+brunaafl@users.noreply.github.com>
2025-03-20 09:14:54 +0100

    Updating gibbs sampler

--- gibbs_sampler/__init__.py
+++ gibbs_sampler/__init__.py
@@ -6,6 +6,7 @@ import numpy as np
 
 from guided_diffusion.gaussian_diffusion import betas_for_alpha_bar
 from util.plot_utils import show_reconstruction
+from util.img_utils import psnr, clear_color
 
 
 def get_named_beta_schedule(schedule_name="linear", num_diffusion_timesteps=1000):
@@ -35,8 +36,8 @@ def get_named_beta_schedule(schedule_name="linear", num_diffusion_timesteps=1000
         raise NotImplementedError(f"unknown beta schedule: {schedule_name}")
 
 class GibbsSampler:
-    def __init__(self, Y, sigma, operator, model, model_type, sampler, device, dsize=(256,256),
-                 N_MC=23, N_bi=20, rho=0.1, rho_decay_rate=0.8, plot_process=None):
+    def __init__(self, X0, Y, sigma, operator, model, model_type, sampler, device, dsize=(256,256),
+                 N_MC=23, N_bi=20, rho=0.1, rho_decay_rate=0.8, plot_process=None, normalize=True):
 
 
         self.N_MC = N_MC
@@ -44,6 +45,7 @@ class GibbsSampler:
         self.rho = rho
         self.rho_decay_rate = rho_decay_rate
         self.device = device
+        self.X0 = X0
         self.Y = Y
         self.sigma = sigma
         self.operator = operator
@@ -52,6 +54,10 @@ class GibbsSampler:
         self.sampler = sampler
         self.plot_process = plot_process
         self.dsize = dsize
+        self.psnr_list = []
+        self.res_list = []
+        self.normxinit = torch.linalg.vector_norm(Y)
+        self.normalize = normalize
 
 
         # Get alphas using the beta schedule
@@ -65,6 +71,10 @@ class GibbsSampler:
         # Initialize X_MC and Z_MC with random values
         self.X_MC[:,:,:,0] = torch.randn((3, *dsize), device=self.device)
         self.Z_MC[:,:,:,0] = torch.randn((3, *dsize), device=self.device)
+
+        if self.model_type == "DnCNN":
+            self.X_MC[:, :, :, 0] = Y.squeeze(0) + 0.01 * torch.randn_like(Y.squeeze(0))
+            self.Z_MC[:, :, :, 0] = Y.squeeze(0) + 0.01 * torch.randn_like(Y.squeeze(0))
 
     def estimate_time(self, value, array=None):
         if array is None:
@@ -80,13 +90,18 @@ class GibbsSampler:
             t_stop = 0
         return t_stop
 
+    def compute_residuals(self, t):
+        res = torch.linalg.vector_norm(self.X_MC[:,:,:,t]-self.X_MC[:,:,:,t-1])/self.normxinit
+        return res
+
     def run(self):
+
         for t in tqdm(range(self.N_MC)):
 
             # Likelihood step
-            self.X_MC[:,:,:,t+1] = self.operator.proximal_generator(
-                self.Z_MC[:,:,:,t], self.Y, self.sigma, rho=self.rho
-            ) # sigma is related to p(y|x)
+            self.X_MC[:, :, :, t + 1] = self.operator.proximal_generator(
+                self.Z_MC[:, :, :, t], self.Y, self.sigma, rho=self.rho
+            ).detach()
 
             # Estimating noise level at each step
             rho_iter = self.rho * (self.rho_decay_rate ** t)
@@ -94,7 +109,6 @@ class GibbsSampler:
             # Prior step
             if self.model_type == 'DDMP':
                 # Update rho and time step
-
                 # Estimating time instant of the forward process
                 t_start = self.estimate_time(rho_iter)
                 t_stop = self.compute_last_diff_step(t_start, t)
@@ -104,22 +118,32 @@ class GibbsSampler:
                     model=self.model,
                     t_start=1000 - t_start,
                     t_end=1000 - t_stop
-                ).squeeze(0)
+                ).squeeze(0).detach()
 
             elif self.model_type == 'DnCNN':
 
                 # The sampling of the auxiliary is
+
                 self.Z_MC[:, :, :, t + 1] = self.sampler.sample(
-                    x=self.X_MC[:,:,:,t+1].unsqueeze(0),
-                    operator=self.operator,
-                    model=self.model, ).squeeze(0)
+                    x=self.X_MC[:,:,:,t+1],
+                    model=self.model, niter=1)
+                """
+                self.Z_MC[:, :, :, t + 1] = self.model(
+                    self.X_MC[:, :, :, t + 1].unsqueeze(0),
+                    sigma=2*self.sigma).squeeze(0).detach()
+                """
 
             if self.plot_process is not None:
                 if (t % self.plot_process == 0) and (t < self.N_bi):
                     # plot the reconstruction on the step t
-                    show_reconstruction(self.Z_MC[:,:,:,t+1], self.X_MC[:,:,:,t+1], t)
+                    show_reconstruction(self.Z_MC[:,:,:,t+1], self.X_MC[:,:,:,t+1], t, normalize=self.normalize)
 
-        if self.model_type == 'DnCNN':
-            self.sampler.plot_residuals()
+            # compute psnr for this step
+            uref = clear_color(self.X0, self.normalize)
+            ut = clear_color(self.X_MC[:, :, :, t + 1], self.normalize)
 
-        return self.X_MC, self.Z_MC
+            ps = psnr(uref, ut)
+            self.psnr_list.append(ps)
+            self.res_list.append(self.compute_residuals(t+1))
+
+        return self.X_MC.detach(), self.Z_MC.detach()
